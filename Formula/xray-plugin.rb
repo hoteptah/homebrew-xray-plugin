567840f33d10fe280dff7eb6e8466bcd0abfd2ab
Amun Ra <amun-ra@vps-1a864300.vps.ovh.ca>
2026-04-22 07:56:43 +0000

    Initial xray-plugin formula

---
+++ Formula/xray-plugin.rb
@@ -0,0 +1,16 @@
+class XrayPlugin < Formula
+  desc "Xray plugin for Shadowsocks"
+  homepage "https://github.com/teddysun/xray-plugin"
+  url "https://github.com/teddysun/xray-plugin/releases/download/v1.8.24/xray-plugin-linux-amd64-v1.8.24.tar.gz"
+  sha256 "8a1881b0ed5c55e429b9842b8343a25bc0d03825d903d8cfbad48a22977f4053"
+  version "1.8.24"
+
+  def install
+    bin.install "xray-plugin"
+  end
+
+  test do
+    system "#{bin}/xray-plugin", "--version"
+  end
+end
+
